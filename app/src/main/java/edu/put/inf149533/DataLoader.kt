2e64323e3a859a8f4ce9102512307c0d5c8c60f9
michal-kowal <kowalewski.michal777@gmail.com>
2023-05-23 00:01:59 +0200

    test1

---
+++ app/src/main/java/edu/put/inf149533/DataLoader.kt
@@ -0,0 +1,110 @@
+package edu.put.inf149533
+
+import android.content.Context
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
+import java.io.File
+import java.io.FileWriter
+import java.net.MalformedURLException
+import java.net.URL
+import javax.xml.parsers.DocumentBuilderFactory
+
+
+
+class DataLoader(val context: Context) {
+    var games:MutableList<Game> = mutableListOf()
+    fun downloadFile(BGCurl : String){
+        println("witam w download")
+        val urlString = BGCurl
+        val xmlDirectory = File("${context.filesDir}/XML")
+        if(!xmlDirectory.exists()) xmlDirectory.mkdir()
+        val fileName = "$xmlDirectory/user.xml"
+        CoroutineScope(Dispatchers.IO).launch {
+            try{
+                val url = URL(urlString)
+                val reader = url.openStream().bufferedReader()
+                val downloadFile = File(fileName).also{it.createNewFile()}
+                val writer = FileWriter(downloadFile).buffered()
+                var line: String
+                while(reader.readLine().also{line=it?.toString()?:""}!=null)
+                    writer.write(line)
+                reader.close()
+                writer.close()
+
+                withContext(Dispatchers.Main){
+                    loadData()
+                    showData()
+                }
+            } catch(e: Exception){
+                withContext(Dispatchers.Main){
+                    when(e){
+                        is MalformedURLException -> print("Malformed URL")
+                        else -> print("Error")
+                    }
+                    val incompleteFile = File(fileName)
+                    if(incompleteFile.exists()) incompleteFile.delete()
+                }
+            }
+        }
+    }
+    fun loadData(){
+        println("witam w loadData")
+        val filename = "user.xml"
+        val path = context.filesDir
+        val inDir = File(path, "XML")
+        if(inDir.exists()){
+            val file = File(inDir, filename)
+            if(file.exists()){
+                val xmlDoc: Document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file)
+                xmlDoc.documentElement.normalize()
+                val items: NodeList = xmlDoc.getElementsByTagName("item")
+                for(i in 0..items.length-1){
+                    val itemNode: Node = items.item(i)
+                    if(itemNode.nodeType==Node.ELEMENT_NODE){
+                        val elem = itemNode as Element
+                        val children = elem.childNodes
+
+                        var id: Long = 0
+                        var title = "N/A"
+                        var originalTitle = "N/A"
+                        var year: Int = 0
+                        var img = "https://ibij.put.poznan.pl/wp-content/uploads/2020/05/putLogoColor.png"
+                        id = elem.getAttribute("objectid").toLong()
+                        for(j in 0..children.length-1){
+                            val node = children.item(j)
+                            if(node is Element){
+                                when(node.nodeName){
+                                    "name" -> {
+                                        originalTitle = node.textContent.toString()
+                                    }
+                                    "yearpublished" -> {
+                                        year = node.textContent.toInt()
+                                    }
+                                    "thumbnail" -> {
+                                        img = node.textContent.toString()
+                                    }
+                                }
+                            }
+                        }
+                        println("sdas")
+                        println(originalTitle)
+                        val game = Game(title,originalTitle,year,id,img)
+                        games.add(game)
+                    }
+                }
+            }
+        }
+    }
+    fun showData(){
+        println("Witam w showdata")
+        for(game in games){
+            println(game.id.toString() + game.originalTitle + game.title + game.img + game.year)
+        }
+    }
+}
---
+++ app/src/main/java/edu/put/inf149533/Game.kt
@@ -0,0 +1,16 @@
+package edu.put.inf149533
+
+class Game {
+    var id: Long = 0
+    var title: String?=null
+    var originalTitle: String?=null
+    var year: Int=0
+    var img: String?=null
+    constructor(title:String, originalTitle:String, year: Int, id: Long, img: String){
+        this.title = title
+        this.originalTitle=originalTitle
+        this.year=year
+        this.id=id
+        this.img=img
+    }
+}
